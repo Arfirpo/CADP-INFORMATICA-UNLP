b4214b5b28fcb2c3fd3ce496e0f0a56c39bd2aab
Agustín Firpo <agustin_firpo@outlook.com.ar>
2024-05-10 01:09:38 -0300

    version completa de prueba - ejercicio 8

--- practica-3/ejercicio8-P3.pas
+++ practica-3/ejercicio8-P3.pas
@@ -1,4 +1,6 @@
+
 Program ejercicio8P3;
+
 
 {La Comision Provincial por la Memoria desea analizar la informacion de los proyectos presentados en el
 programa Jovenes y Memoria durante la convocatoria 2020. 
@@ -10,8 +12,7 @@ Cada escuela puede presentar mas de un proyecto.
 
 La informacion se ingresa ordenada consecutivamente por localidad y, para cada localidad, por escuela. 
 
-Realizar un programa que lea la informacion de los proyectos hasta que se ingrese el proyecto con codigo -1 (que no
-debe procesarse).
+Realizar un programa que lea la informacion de los proyectos hasta que se ingrese el proyecto con codigo -1 (que no debe procesarse).
 
 informe:
 
@@ -27,10 +28,10 @@ Type
     dni: str50;
     nomYape: str50;
     email: str50;
-  end;
+  End;
   {registro de proyectos}
   proyecto = Record
-    cod: cod;
+    cod: cdg;
     part: integer;
     doc: coordinador;
     tit: str50;
@@ -38,37 +39,47 @@ Type
     loc: str50;
   End;
 
-{compara, entre centros, las dos con menos becarios
-Procedure centMinBeca(nombre: str50; cantBec: integer; Var min1,min2: integer; Var minCentro1,minCentro2: str50);
+
+{compara, entre escuelas, las dos con mas alumnos participantes de proyectos}
+Procedure escMaxAlu(escuela: str50; alumnos: integer; Var max1,max2: integer; Var escMax1,escMax2: str50);
 Begin
-  If cantBec < min1 Then
+
+  If alumnos > max1 Then
     Begin
-      min2 := min1;
-      minCentro2 := minCentro1;
-      min1 := cantBec;
-      minCentro1 := nombre;
+      max2 := max1;
+      escMax2 := escMax1;
+      max1 := alumnos;
+      escMax1 := escuela;
     End
   else
-    if cantBec < min2 Then
+    if alumnos > max2 Then
       Begin
-        min2 := cantBec;
-        minCentro2 := nombre;
+        max2 := alumnos;
+        escMax2 := escuela;
       end;
 End;
 
-compara entre universidades y devuelve la universidad con más investigadores
-Procedure uniMaxInvest(universidad: str50; cantInvest: integer; Var max1: integer; Var maxUni: str50);
+{analiza cada digito del codigo de un proyecto y cuenta cuantos digitos pares e impares tiene}
+Procedure procDig(titulo: str50; codigo: cdg);
+var
+  digPar,digImp, digito: integer;
 Begin
-  If cantInvest > max1 Then
-    Begin
-      max1 := cantInvest;
-      maxUni := universidad;
-    End
+digPar := 0; digImp := 0;
+while digito <> 0 do
+  begin
+    digito := codigo mod 10;
+    if ((digito mod 2) = 0 ) then
+      digPar := digPar +1;
+    else
+      digImp := digImp +1;
+    digito := codigo div 10;
+  end;
+if digPar = digImp then
+  writeln(titulo,' posee un codigo con misma cantidad de digitos pares e impares')
 End;
 
-lee por teclado los datos del registro: coordinador}
-
-Procedure leerCoordinador(var c: coordinador);
+{lee por teclado los datos del registro: coordinador}
+Procedure leerCoordinador(Var c: coordinador);
 Begin
   write('Ingrese el DNI del coordinador: ');
   readln(c.dni);
@@ -80,10 +91,10 @@ End;
 
 
 {lee por teclado los datos del registro: proyecto}
-Procedure leerProyecto(Var p: proyecto; var c: coordinador);
+Procedure leerProyecto(Var p: proyecto; Var c: coordinador);
 Begin
-      write('Ingrese el codigo del proyecto: ');
-      readln(p.cod);
+  write('Ingrese el codigo del proyecto: ');
+  readln(p.cod);
   If p.codigo <> -1 Then
     Begin
       write('Ingrese el titulo del proyecto: ');
@@ -98,41 +109,48 @@ Begin
     End;
 End;
 
+
+{programa principal}
+
 Var 
   p: proyecto; c: coordinador;
-  escTot,escLoc: integer;
+  escTot,escLoc,cantAlu,max1,max2: integer;
+  escuela,localidad,escMax1,escMax2: str50;
 Begin
   // SECTOR 1- ACA VAN TODAS LAS VARIABLES QUE SE USAN PARA CONTABILIZAR LOS TOTALES ENTRE TODOS LOS DATOS LEIDOS
-  escTot := 0;
-
+  escTot := 0; max1 := 0; max2 := 0;
+  escMax1 := ''; escMax2 := '';
   leerProyecto(p,c);
 
   While p.cod <> 0 Do
     Begin
       // SECTOR 2- ACA VAN TODAS LAS VARIABLES QUE SE DEBEN INICIALIZAR CUANDO CAMBIA DE LOCALIDAD (OSEA CUANDO TE PIDEN ALGO POR LOCALIDAD)
       escLoc := 0;
-      localidad := p.loc; //corte de control x localidad
-
-
+      localidad := p.loc;
+      //corte de control x localidad
       // SECTOR 3- ACA SE VAN A ESTAR PROCESANDO TODOS LOS DATOS POR LOCALIDAD (EN ESTE CASO TODAS LAS ESCUELAS)
       While (p.loc = localidad) And (p.cod <> 0) Do
         Begin
           // SECTOR 4- ACA SE VAN A ESTAR PROCESANDO TODOS LOS DATOS POR ESCUELA (EN ESTE CASO CADA ESCUELA)
           escTot := escTot +1;
           escLoc := escLoc +1;
+          cantAlu := 0;
           escuela := p.esc; //corte de control x escuela
-          While(p.esc = escuela) do
-            begin
-              
-            end;
+          if p.loc = 'daireaux' then
+            procDig(p.tit,p.cod);
+          While (p.esc = escuela) And (p.cod <> 0) Do
+            Begin
+              cantAlu := cantAlu + p.part;
+              leerProyecto(p,c);
+            End;
           // SECTOR 5- ACA VA TODO LO QUE ME PIDEN ACTUALIZAR O INFORMAR POR ESCUELA
-          writeln();
+          escMaxAlu(escuela,cantAlu,max1,max2,escMax1,escMax2);
         End;
       // SECTOR 6- ACA VA TODO LO QUE ME PIDEN ACTUALIZAR O INFORMAR POR LOCALIDAD
       writeln('La localidad ',localidad,' tiene ',escLoc,' escuelas');
     End;
 
-
   // SECTOR 7- ACA INFORMO TODO LO QUE ME PIDEN EN GENERAL Y SIN QUE DEPENDA DEL CORTE DE CONTROL DE LOCALIDAD
   writeln(escTot,' escuelas presentaron proyectos en el año 2020');
-End.b
+  writeln('Las dos escuelas con mayor numero de alumnos participantes fueron: 1.',escMax1,' 2.',escMax2);
+End.
