5c925d9b49e8291e59a8e0f9e78db91a23b6f116
Agustín Firpo <agustin_firpo@outlook.com.ar>
2024-05-11 18:14:19 -0300

    ejercicio 3 completo

--- practica-4.pas/ejercicio3-P4.pas
+++ practica-4.pas/ejercicio3-P4.pas
@@ -1,45 +1,143 @@
 Program ejercicio2P4;
 
 const
-  dimF = 5;
+  dimF = 8;
 
 Type 
   vNum = array [1..dimF] of integer;
 
+
+{procedimiento para cargar el vector}
 procedure cargarVector(var v: vNum; var dimL: integer);
 Var
-  num: real;
+  num: integer;
+  i: integer;
 Begin
   dimL := 0;
-  write('Ingrese un numero: ');
-  read(num);
-  for dimL := 1 to dimF do
+  for i := 1 to dimF do
     begin
-      v[dimL] := num;
-      dimL := diml +1;
       write('Ingrese un numero: ');
       read(num);
+      v[i] := num;
+      dimL := diml +1;
     end;
 end;
 
-procedure imprimirTot(num: vNum; dimL: integer);
+procedure cargarXY(var x,y: integer);
+begin
+  write('Ingrese la posicion X: ');
+  readln(x);
+  write('Ingrese la posicion Y: ');
+  readln(y);
+  writeln('X vale: ',x,' e Y vale: ',y);
+end;
+
+{a. procedimiento para imprimir el vector completo}
+procedure imprimirTot(v: vNum);
 Var
   i: integer;
 Begin
-  for i := 1 to dimL do
-    write(num[i]:0:2,' ');
+  for i := 1 to dimF do
+    write(v[i],' ');
     writeln(' ');
 end;
 
+
+{b. procedimiento para imprimir el vector completo, del ultimo al primero}
+procedure imprimirTotInv(v: vNum);
+var
+  i: integer;
+begin
+  for i := dimF downto 1 do
+    write(v[i],' ');
+  writeln(); 
+end;
+
+{c. procedimiento para imprimir el vector desde la mitad, hasta la primer posicion}
+{c. procedimiento para imprimir el vector desde la mitad + 1, hasta la ultima posicion}
+procedure imprimirMid(v: vNum; dimL: integer);
+var
+  i: integer;
+begin
+  for i:= (dimL div 2) downto 1 do
+    write(v[i],' ');
+  writeln();
+
+  for i:= ((diml div 2) + 1) to dimL do
+    write(v[i],' ');
+    writeln(); 
+end;
+
+
+{d. procedimiento para imprimir el vector desde x hasta y, o desde y hasta x}
+procedure imprimirXY(v: vNum;x,y: integer);
+var
+  i: integer;
+begin
+  cargarXY(x,y);
+  if x < y then
+      for i:= x to y do
+      begin
+        write(v[i],' ');        
+      end
+  else 
+    if x > y then
+      for i:= x downto y do
+        begin
+          write(v[i],' ');
+        end;
+end;
+
+{e. utilizando el modulo del inciso d. para rehacer los incisos a., b. y c.}
+procedure imprimirXYplus1(v: vNum; dimL,x,y: integer);
+var
+  i: integer;
+begin
+  x := 1;
+  y := dimL;
+  {ejercicio a. rehecho}
+  for i:= x to y do
+    begin
+      write(v[i],' ');        
+    end;
+
+  writeln();
+
+  {ejercicio b. rehecho}
+  for i:= y downto x do
+    begin
+      write(v[i],' ');
+    end;
+
+  writeln();
+
+  {ejercicio c. rehecho}
+  for i:= y div 2 downto x do
+    begin
+      write(v[i],' ');
+    end;
+
+  writeln();
+  
+  for i:= ((y div 2) + x) to y do
+    write(v[i],' ');
+    writeln();  
+end;
 
 
 
 {programa principal}
 Var 
-  dimL: integer;
-  num: vNum;
+  dimL,x,y: integer;
+  v: vNum;
 Begin
   dimL := 0;
-  cargarVector(num,dimL);
-  imprimirTot()
+  x := 0;
+  y := 0;
+  cargarVector(v,dimL);
+  // imprimirTot(v);
+  // imprimirTotInv(v);
+  // imprimirMid(v,dimL);
+  // imprimirXY(v,x,y)
+  imprimirXYplus1(v,dimL,x,y)
 End.
