2265d473fe0f25be6eb4b641f0d4f51c6d96b7b7
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-05-15 15:29:41 -0300

    continuar ejercicio 13

---
+++ practica-4/ejercicio13-P4.pas
@@ -0,0 +1,32 @@
+program ejercicio13P4;
+
+const
+  pDeMedicion = 100; //puntos de medicion en el planeta.
+  aDeRelev = 50; //años de relevamiento
+
+type
+  str30 = string[30];
+  rango = 1..pDeMedicion;
+  rRelev = 1..aDeRelev;
+
+  punto = record
+    lugar: str30;
+    tProm: real;
+  end;
+
+  vPuntos = array[rango] of punto;    //vector de registros de puntos 
+  vRelev = array[rRelev] of vPuntos;  //vector que guarda: vectores que almacenan registros de puntos.
+
+  {modulos}
+
+
+
+
+
+  {programa principal}
+  var
+    p: vPuntos;
+    r: vRelev;
+  begin
+    cargarV()
+  end.
