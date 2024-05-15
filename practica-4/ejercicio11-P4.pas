4d4725d9793b20fddfd51e79ea8fc0543715586c
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-05-15 09:16:03 -0300

    ejercicio 11 completo

--- practica-4/ejercicio11-P4.pas
+++ practica-4/ejercicio11-P4.pas
@@ -1,8 +1,8 @@
 program ejercicio11P4;
 
 {constantes globales}
-
-dimF = 200   //cantidad total de fotos publicadas en ArgenPics.
+const
+    dimF = 4;   //cantidad total de fotos publicadas en ArgenPics.
 
 {tipos de datos definidos por el programador}
 
@@ -41,7 +41,9 @@ begin
   readln(pic[i].com);
 end;
 
-//Procedimiento que recibe un vector "vFotos" vacio y, mediante una estructura for, lo carga hasta el total de la dim. Fisica con registros tipo foto.
+{Procedimiento que recibe dos vectores: "pic"(registros) y c(contador) vacios y mediante una estructura for 
+se carga el primero y se inicializa el segundo en cero, hasta el total de la dim. Fisica.}
+
 procedure cargEinicVector(var pic: vFotos; var c: vComent);
 var
   i: rango;
@@ -62,34 +64,44 @@ begin
     end;    
 end;
 
-procedure recorrerVector(pic: vFotos; var c: vComent);
+procedure recorrerVector(pic: vFotos; var c: vComent; var cantMg, max: integer; var maxTit: str30);
 var
   i: rango;
-  max, cantMg: integer;
-  maxTit: string;
-begin
-  max := -1;
-  maxTit := '';
-  cantMg := 0;
+begin  
   for i := 1 to dimF do
     begin
       //a.calcula cant. de clicks de cada foto y devuelve el titulo de la foto mas vista;
       masVista(pic[i].clicks,pic[i].tit,max,maxTit);
       //b.si el autor de la foto es igual al buscado, sumamos +1 al contador de me gustas.   
       if (pic[i].aut = 'art vandelay') then
-        cantMg := cantMg +1;
+        cantMg := cantMg + pic[i].mgs;
       //c. voy asignandole al vector contador, por indice, la cantidad de comentarios de cada registro foto.
       c[i] := pic[i].com;
     end;
 end;
 
-
+procedure informarVector(pic: vFotos; c: vComent; cantMg: integer; maxTit: str30);
+var
+  i: integer;
+begin
+  writeln('la foto mas vista de la pagina es "',maxTit,'"');
+  writeln('Las fotos del autor Art Vandelay tuvieron un total de ',cantMg,' Me gusta');
+  for i := 1 to dimF do
+    writeln('La foto "', pic[i].tit,'" tuvo ',c[i],' comentarios');
+end;
 
 {programa principal}
 var
   pic: vFotos;
   c: vComent;
+  cantMg: integer;
+  max: integer;
+  maxTit: string;
 begin
+  max := -1;
+  maxTit := '';
+  cantMg := 0;
   cargEinicVector(pic,c);
-  recorrerVector(pic,c);
-end;
+  recorrerVector(pic,c,cantMg,max,maxTit);
+  informarVector(pic,c,cantMg,maxTit);
+end.
