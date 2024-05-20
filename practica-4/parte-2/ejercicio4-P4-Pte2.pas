7c71dc6a7f67bbd50fb36a0f38106f0e22e04a89
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-05-20 14:11:17 -0300

    ejercicio 4 hasta punto b. terminado

--- practica-4/parte-2/ejercicio4-P4-Pte2.pas
+++ practica-4/parte-2/ejercicio4-P4-Pte2.pas
@@ -72,20 +72,6 @@ begin
   end;
 end;
 
-procedure cargarV(var a: vAlu; var dimL: integer; var codigos: codUsados);
-var
-  alu: alumno;
-begin
-  while (dimL < dimF) do
-  begin
-    leerAlu(alu, codigos);
-    if (alu.cod >= 1) and (alu.cod <= dimF) then
-    begin
-      insertarOrdenado(a, dimL, alu);
-    end;
-  end;
-end;
-
 procedure imprimirV(a: vAlu; dimL: integer);
 var
   i: integer;
@@ -100,34 +86,76 @@ begin
     end;
 end;
 
-function buscarAlu(a: vAlu; dimL: integer):integer;
+procedure cargarV(var a: vAlu; var dimL: integer; var codigos: codUsados);
 var
-  i,cod,inf,med,sup: integer;
+  alu: alumno;
 begin
-  write('Ingrese codigo del alumno: ');
-  readln(cod);
-  inf := 1; sup := dimL; med := (inf + sup div 2);
-  if cod >= 1 and cod <= dimF then
-    for i := 1 to dimL do
+  while (dimL < dimF) do
+  begin
+    leerAlu(alu, codigos);
+    if (alu.cod >= 1) and (alu.cod <= dimF) then
     begin
-      
+      insertarOrdenado(a, dimL, alu);
     end;
+  end;
+  {imprimirV(a,dimL);}
+end;
 
+
+function buscarAlu(a: vAlu; dimL: integer; valor: integer):integer;
+var
+  pos: integer;
+begin
+  pos := 1;
+
+  while ((pos < dimL) and (a[pos].cod < valor)) do
+  begin
+    pos := pos +1;
+  end;
+  if ((pos < dimL) and (a[pos].cod = valor)) then
+    buscarAlu := pos
+  else
+    buscarAlu := -1;
+end;
+
+procedure insertarEnV(var a: vAlu; alu: alumno var dimL: integer; var pude: boolean);
+var
+  i: integer;
+begin
+  pude := false;
+  if((dimL +1) < dimF) and (alu.cod > 1) and (alu.cod < dimL) then
+  begin
+    for i := dimL to alu.cod do
+      a[i+1] := a[i];
+    pude := true;
+    a[alu.cod] := alu;
+    dimL dimL +1;
+  end;
 end;
 
 { Programa principal }
 var
   a: vAlu;
+  alu: alumno;
   codigos: codUsados;
-  dimL: integer;
+  dimL,cod: integer;
   i, posAlu: integer;
+  pude: boolean;
 begin
   Randomize;
   dimL := 0;
   inicializarCodigos(codigos);
   cargarV(a, dimL, codigos);
-  {imprimirV(a,dimL);}
-  posAlu := buscarAlu(a,dimL);
+
+  {ejercicio 4 inciso a.}
+  write('Ingrese codigo del alumno a buscar: ');
+  readln(cod);
+  posAlu := buscarAlu(a,dimL,cod);
+  if posAlu <> -1 then
+    writeln('El alumno buscado se encuentra en la posicion ',posAlu,' y su nombre es ',a[posAlu].nomYape);
+  {ejercicio 4 inciso b.}
+  leerAlu(alu,codigos);
+  insertarEnV(a,alu,dimL,pude);
 end.
 
 
