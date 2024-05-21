2d79808218ac8eda09cadc3c3005a835263a23ac
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-05-21 12:18:51 -0300

    ejercicio 4 completo (sin verificar!)

--- practica-4/parte-2/ejercicio4-P4-Pte2.pas
+++ practica-4/parte-2/ejercicio4-P4-Pte2.pas
@@ -51,7 +51,7 @@ begin
     until not codigos[cod];
     codigos[cod] := True;
     randomString(10, nomYape);
-    asis := Random(50) + 1; // Genera un número aleatorio entre 1 y 50
+    asis := Random(51) - 1; // Genera un número aleatorio entre -1 y 50
   end;
 end;
 
@@ -101,35 +101,112 @@ begin
   {imprimirV(a,dimL);}
 end;
 
-
+{ejercicio 4.a.}
 function buscarAlu(a: vAlu; dimL: integer; valor: integer):integer;
 var
   pos: integer;
 begin
   pos := 1;
-
   while ((pos < dimL) and (a[pos].cod < valor)) do
   begin
     pos := pos +1;
   end;
-  if ((pos < dimL) and (a[pos].cod = valor)) then
-    buscarAlu := pos
-  else
-    buscarAlu := -1;
+  if ((pos > dimL) or (a[pos].cod <> valor)) then
+    pos := 0;
+  buscarAlu := pos;
 end;
 
-procedure insertarEnV(var a: vAlu; alu: alumno var dimL: integer; var pude: boolean);
+{ejercicio 4.b.}
+Procedure InsertarElemOrd(var a:vAlu; var dimL:integer; alu:alumno; var exito:boolean);
+
+  Procedure Insertar(var a:vAlu; var dimL:integer; pos: integer; alu: alumno);
+    var
+      j: indice;
+    begin
+      for j:= dimL downto pos do
+        a [j+1] := a [j] ;
+        a [pos] := alu;
+        dimL := dimL+1;
+    End;
 var
-  i: integer;
+  pos: integer;
+
+Begin
+  exito := false;
+  if (dimL < dimF) then begin
+    pos := buscarAlu(a,dimL,alu.cod);
+    Insertar (a, dimL, pos, alu);
+    exito := true;
+  end;
+end;
+
+{ejercicio 4.c.}
+Procedure BorrarPos (var a: vAlu; var dimL: integer; pos: integer; var exito: boolean );
+var
+  i: integer; 
 begin
-  pude := false;
-  if((dimL +1) < dimF) and (alu.cod > 1) and (alu.cod < dimL) then
+  exito := false;
+  if (pos >=1 and pos <= dimL)then begin   //verifica que sea una posicion valida
+    exito := true;
+    for i:= pos+1 to dimL do  //hace el corrimiento para la izquierda
+      a[i-1] := a[i];
+    dimL := dimL-1 ;   //resta uno a la cant de elementos que tiene el vector
+  end;
+end;
+
+{ejercicio 4.d.}
+Procedure BorrarElem (var a: vAlu;  var dimL: integer; cod: integer; var exito: boolean);
+
+  Procedure BorrarPosModif (var a: vAlu; var dimL:integer; pos:integer);
+  var
+    i: integer; 
   begin
-    for i := dimL to alu.cod do
-      a[i+1] := a[i];
-    pude := true;
-    a[alu.cod] := alu;
-    dimL dimL +1;
+    for i:=pos+1 to dimL  do
+      a[i-1] := a[i];
+    dimL := dimL-1;
+
+var
+  pos: integer;
+Begin
+  exito:= False;
+  pos:= BuscarPosElem (a,dimL,cod);
+  if pos <> 0 then begin
+    BorrarPosModif(v,dimL,pos);
+    exito:= true;
+  end;
+end;
+
+{ejercicio 4.e.}
+procedure BorrarPorAsis(var vector: vAlu; var dimL: integer; var verificador: boolean);
+var
+  pos: integer;
+  asis: integer;
+begin
+  asis := 0;
+  elim := false;
+  pos := 1;
+  while (pos <= dimL) do
+  begin
+    if (vector[pos].asis = asis) then
+    {el codigo dentro de aca, puede ser un procedure "eliminar" aparte}
+    begin
+      if (pos >= 1) and (pos <= dimL) then
+      begin
+        for i := pos to (dimL - 1) do
+          v[i] := v[i + 1];
+        elim := true;
+        dimL := dimL - 1;
+      end;
+    {----------------------------------------------------} 
+      if elim then
+      begin
+        elim := false;        
+      end
+      else
+        i := i + 1;
+    end
+    else
+      i := i + 1;
   end;
 end;
 
@@ -139,8 +216,8 @@ var
   alu: alumno;
   codigos: codUsados;
   dimL,cod: integer;
-  i, posAlu: integer;
-  pude: boolean;
+  i, posAlu,pos: integer;
+  pude,exito: boolean;
 begin
   Randomize;
   dimL := 0;
@@ -151,14 +228,19 @@ begin
   write('Ingrese codigo del alumno a buscar: ');
   readln(cod);
   posAlu := buscarAlu(a,dimL,cod);
-  if posAlu <> -1 then
+  if posAlu <> 0 then
     writeln('El alumno buscado se encuentra en la posicion ',posAlu,' y su nombre es ',a[posAlu].nomYape);
   {ejercicio 4 inciso b.}
   leerAlu(alu,codigos);
-  insertarEnV(a,alu,dimL,pude);
+  InsertarElemOrd(a,alu,dimL,pude);
+  {ejercicio 4 inciso c.}
+  write('Ingrese posicion: ');
+  readln(pos);
+  BorrarPos(a,dimL,pos,exito);
+  {ejercicio 4 inciso d.}
+  write('Ingrese codigo de Alumno: ');
+  readln(cod);
+  BorrarPorCod(a,dimL,cod,exito);
+  {ejercicio 4 inciso e.}
+  BorrarPorAsis(a,dimL,exito);
 end.
-
-
-
-
-
