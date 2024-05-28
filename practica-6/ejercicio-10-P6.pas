43294263021801ab50dcc6b6307983320a8e3bcf
Agustín R. Firpo <135714553+Arfirpo@users.noreply.github.com>
2024-05-28 14:22:15 -0300

    terminar ejercicio 10 P6

--- practica-6/ejercicio-10-P6.pas
+++ practica-6/ejercicio-10-P6.pas
@@ -5,8 +5,7 @@ número (que es el siguiente al último número entregado).
 El cliente quedará esperando a ser llamado por su número, en cuyo caso sale de la lista de espera. 
 Se pide:
     a. Definir una estructura de datos apropiada para representar la lista de espera de clientes.
-    b. Implementar el módulo RecibirCliente, que recibe como parámetro el DNI del cliente y la lista de clientes en espera, 
-    asigna un número al cliente y retorna la lista de espera actualizada.
+    b. Implementar el módulo RecibirCliente, que recibe como parámetro el DNI del cliente y la lista de clientes en espera, asigna un número al cliente y retorna la lista de espera actualizada.
     c. Implementar el módulo AtenderCliente, que recibe como parámetro la lista de clientes en espera,
     y retorna el número y DNI del cliente a ser atendido y la lista actualizada. El cliente atendido debe
     eliminarse de la lista de espera.
@@ -33,35 +32,81 @@ type
 
 {modulos}
 
-procedure leerCliente(var c: cliente,t:integer);
+{b. Implementar el módulo RecibirCliente, que recibe como parámetro el DNI del cliente y la lista de clientes en espera}
+procedure recibirCliente(var l: lClientes; dni: integer);
+
+  //a. Definir una estructura de datos apropiada para representar la lista de espera de clientes.
+  procedure agregarAlFinal(var l: lClientes; c: cliente);
+  var
+    aux,nuevo: lClientes;
+  begin
+    new(nuevo);
+    nuevo^.dato := c;
+    nuevo^.sig := nil;
+
+    if (l = nil) then
+      l := nuevo
+    else
+    begin
+      aux := l;
+      while (aux^.sig <> nil) do
+        aux := aux^.sig;
+      aux^.sig := nuevo;
+    end;
+  end;
+
+var
+  c: cliente; t: integer;
 begin
-  with c do begin
-    write('Ingrese el Dni del Cliente: ');
+  t := 0;
+  while dni <> 0 do begin
+    c.dni := dni;
+    t := t + 1;
+    c.turno := t;
+    agregarAlFinal(l,c);
+    write('Ingrese su numero de documento: ');
     readln(dni);
-    turno := t;
   end;
 end;
 
-procedure listaDeEspera(var l: lClientes);
-var
-  t: integer; c: cliente;
-begin
-  t := 1;
-  leerCliente(c,t);
-  while c.dni <> 0 do begin
-    agregarAlFinal(l,c);
-    t := t + 1;
-    leerCliente(c,t);
-  end;
-end;
+
+{programa principal}
 
 var
   lEspera: lClientes;
-
+  dni: integer;
 
 begin
   lEspera := nil;
-  listaDeEspera(lEspera);
+  write('Ingrese su numero de documento: ');
+  readln(dni);
+  recibirCliente(lEspera,dni);
+
+
+
 end.
 
 
+
+{procedure eliminarAtendido(var l: lClientes; pos: integer);
+var
+  ant,act: lClientes;
+begin
+  
+  act := l;
+  while act^.dato.turno <> pos do begin
+    ant := act;
+    act := act^.sig;
+  end;
+  
+  if act = l then
+    l := act^.sig;
+  dispose(act);
+end;
+
+procedure atenderCliente(l: lClientes; var dniAt,turnoAt: integer);
+begin
+  dniAt := l^.dato.dni;
+  turnoAt := l^.dato.turno;
+  writeln('Siguiente: Turno N° ',dniAt,' Dni ',turnoAt);  
+end;}
