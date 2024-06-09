6383781e8c02193ee1a83e4c3928ca32a4e55b0f
Agustín Firpo <agustin_firpo@outlook.com.ar>
2024-06-09 02:09:54 -0300

    parcial 2024 tema 2 completo

--- parciales viejos/parcialTren.pas
+++ parciales viejos/parcialTren.pas
@@ -1,152 +1,196 @@
-{Una empresa de venta de tickets de tren esta analizando la informacion de los viajes realizados por sus trenes durante el año 2022.
-Para ello se conoce el codigo de tren, el mes en que se realizo el viaje (entre 1 y 12), la cantidad de pasajeros que viajaron, y el codigo de la ciudad de destino (entre 1 y 20).
-La información se encuentra ordenada por codigo de tren.
-Ademas, la empresa dispone de una estructura de datos con la informacion de costo del ticket por ciudad de destino.
-Realizar un programa que procese la informacion de los viajes y:
-  A.Genere una lista que tenga por cada codigo del tren, la cantidad de viajes realizados.
-  B.Informe el mes con mayor monto recaudado.
-  C.Informe el promedio de pasajeros por cada tren entre todos sus viajes}
+{}
 
-program parcial2024;
+program nombrePrograma;
+
+uses
+  sysUtils;
 
 const
-  dimF_mes = 12;
-  dimF_cd = 20;
+  dimF_marca = 130;
+
 type
-  rango_meses = 1..dimF_mes;
-  rango_cd = 1..dimF_cd;
+  r_marca = 1..dimF_marca;
   str30 = string[30];
 
-  lViajes = ^nViajes;
+  lRepuestos = ^nRepuestos;
 
-  lTrenes = ^nTrenes;
-
-  viaje = record
-    codT: integer;
-    mesV: rango_meses;
-    cantPas: integer;
-    codCD: rango_cd;
+  repuesto = record
+    cod: integer;
+    prec: real;
+    codM: r_marca;
+    nomP: str30;
   end;
 
-  tren = record
-      cod: integer;
-      cantViajes: integer;
+  marca = record
+    cod: r_marca;
+    nom: str30;
+    minPrec: real;
   end;
 
-  nTrenes = record
-    dato: tren;
-    sig: lTrenes;
-  end;
+  vMarcas = array[r_marca] of marca;
 
-  nViajes = record
-    dato: lViajes;
-    sig: lViajes;
+  nRepuestos = record
+    dato: repuesto;
+    sig: lRepuestos;
   end;
-
-  meses = record
-    mRec: real;
-    mes: str30;
-  end;
-
-  vPrecios = array[rango_cd] of real; //se dispone
-  vMeses = array[rango_meses] of meses;
 
 {modulos}
 
-procedure inicializarVectorPrecios(var vP: vPrecios); // se dispone.
-
-procedure inicializarVectorMeses(var vM: vMeses);
-var 
-  i: rango_meses;
+procedure randomString(StringLen: integer; var palabra: str30);
+var
+  str: string;
+  Result: str30;
 begin
-  for i := 1 to dimF_mes do
-    vM[i].mRec := 0;
-    case i of
-      1: vM[i].mes := 'Enero';
-      2: vM[i].mes := 'Febrero';
-      3: vM[i].mes := 'Marzo';
-      4: vM[i].mes := 'Abril';
-      5: vM[i].mes := 'Mayo';
-      6: vM[i].mes := 'Junio';
-      7: vM[i].mes := 'Julio';
-      8: vM[i].mes := 'Agosto';
-      9: vM[i].mes := 'Septiembre';
-      10: vM[i].mes := 'Octubre';
-      11: vM[i].mes := 'Noviembre';
-      12: vM[i].mes := 'Diciembre';
-    end;
+  str := 'abcdefghijklmnopqrstuvwxyz';
+  Result := '';
+  repeat
+    Result := Result + str[Random(Length(str)) + 1];
+  until Length(Result) = StringLen;
+  palabra := Result;
 end;
 
-procedure generarListaViajes(var l: lViajes); // se dispone.
-
-procedure procesarListaViajes(l: lViajes; vP: vPrecios; var vM: vMeses);
-
-  procedure agregarAdelante(var l: lTrenes; cantViajes: integer);
-  var
-    nue: lTrenes;
-  begin
-    new(nue);
-    nue^.dato := cantViajes;
-    nue^.sig := l;
-    l := nue;
-  end;
-
-  function maxrec(vM: vMeses):integer;
-  var
-    max: real;
-    mes: integer;
-    i: rango_meses;
-  begin
-    max := vM[1].mRec;
-    mes := 1;
-    for i := 2 to dimF_mes do begin
-      if vM[i].mRec > max then
-        max := vM[i].mRec;
-        mes := i;
-    end;
-    maxrec := mes;
-  end;
-
-  function promPas(pasajeros,viajes: integer):real;
-  begin
-    promPas := pasajeros / viajes;
-  end;
-
-var
-  aux: lViajes;
-  pri: lTrenes;
-  corte_tren: integer;
-  cantViajes,totPas: integer;
+procedure leerRepuesto(var r: repuesto);
 begin
-  aux := l;
-  pri := nil;
-  while (aux <> nil) do begin
-    corte_tren := aux^.dato.codT;
-    cantViajes := 0;
-    totPas := 0;
-    while (aux <> nil) and (aux^.dato.codT = corte_tren) do begin
-      cantViajes := cantViajes + 1;
-      totPas := totPas + aux^.dato.cantPas;
-      vM[aux^.dato.mesV].mRec := vM[aux^.dato.mesV].mRec + (aux^.dato.cantPas * vP[aux^.dato.codCD]);
-      aux := aux^.sig;
+  with r do begin
+    cod := Random(9999) - 1;
+    if (cod <> -1) then begin
+      prec := Random(999) + 1;
+      codM := Random(dimF_marca) + 1;
+      randomString(5,nomP);
     end;
-    agregarAdelante(pri,cantViajes);
-    writeln('El promedio de pasajeros para el tren Nro. ',corte_tren,' fue de aproximadamente ',promPas(totPas,cantViajes):0:2,' pasajeros por viaje');
   end;
-  writeln('El mes con mayor monto recaudado fue: ',vM[maxrec(vM)].mes);
+end;
+
+procedure insertarOrdenado(var l: lRepuestos; r: repuesto);
+var
+  nue,ant,act: lRepuestos;
+begin
+  new(nue);
+  nue^.dato := r;
+  ant := l;
+  act := l;
+  while (act <> nil) and (r.nomP > act^.dato.nomP) do begin
+    ant := act;
+    act := act^.sig;
+  end;
+  if (act = ant) then
+    l := nue
+  else
+    ant^.sig := nue;
+  nue^.sig := act;
+end;
+
+procedure generarLista(var l:lRepuestos); //se dispone
+var
+  r: repuesto;
+begin
+  leerRepuesto(r);
+  while (r.cod <> -1) do begin
+    insertarOrdenado(l,r); //se ordena por nombre de pais.
+    leerRepuesto(r);
+  end;
+end;
+
+procedure cargarVector(var vM: vMarcas);
+var
+  i: r_marca;
+begin
+  for i := 1 to dimF_marca do begin
+    randomString(5,vM[i].nom);
+    vM[i].cod := i;
+    vM[i].minPrec := 9999;
+  end;
+end;
+
+function cumple3(codigo: integer):boolean;
+var
+  dig: integer;
+  ok: boolean;
+begin
+  ok := true;
+  while (codigo <> 0) do begin
+    dig := codigo mod 10;
+    if (dig = 0) then begin
+      ok := false;
+      codigo := 0;
+    end
+    else
+      codigo := codigo div 10;
+  end;
+  cumple3 := ok;
+end;
+
+function precMin(precio,min: real):real;
+begin
+  if (precio < min) then
+    min := precio;
+
+  precMin := min;
+end;
+
+procedure informarB2(vM: vMarcas);
+var
+  i: r_marca;
+begin
+  for i := 1 to dimF_marca do begin
+    writeln('Nombre de la Marca: ',vM[i].nom);
+    writeln('Precio del producto mas barato: ',vM[i].minPrec:0:2);
+    writeln('----------------------------------------------');
+  end;
+    
+end;
+
+procedure procesarLista(l: lRepuestos; var vM: vMarcas);
+var
+  corte_nomP: str30;
+  cantR,cantP,pCumple3: integer;
+begin
+  cantP := 0; pCumple3 := 0;
+  while (l <> nil) do begin
+    corte_nomP := l^.dato.nomP;
+    cantR := 0;
+    while (l <> nil) and (l^.dato.nomP = corte_nomP) do begin
+      //cuento cantidad de repuestos comprados por pais.
+      cantR := cantR + 1;
+      //actualizo el precio minimo por marca.
+      vM[l^.dato.codM].minPrec := precMin(l^.dato.prec,vM[l^.dato.codM].minPrec);
+      //cuento paises que cumplen condicion 3.
+      if(cumple3(l^.dato.cod)) then
+        pCumple3 := pCumple3 + 1;
+      //avanzo a otro repuesto.
+      l := l^.sig;
+    end;
+    //al procesar un pais, evaluo y cuento si ese pais cumple condicion b.1.
+    if(cantR > 100) then
+      cantP := cantP + 1;
+  end;
+  //informo b.1.
+  writeln('----------------------------------------------------------');
+  if cantP > 0 then
+    writeln('A ',cantP,' pais/es se les compro mas de 100 repuestos.')
+  else
+    writeln('A ningun pais se le compro mas de 100 repuestos.');
+  writeln('----------------------------------------------------------');
+  //informo b2
+  informarB2(vM);
+  //informo b.3.
+  if (pCumple3 > 0) then
+    writeln(pCumple3,' repuesto/s no poseen ningun cero en su codigo.')
+  else
+    writeln('Ningun repuesto no posee ningun cero en su codigo.');
+  writeln('----------------------------------------------------------');
 end;
 
 
 {programa principal}
-
 var
-  pri: lViajes;
-  vP: vPrecios;
-  vM: vMeses;
+  pri: lRepuestos;
+  vM: vMarcas;
+
 begin
+  Randomize;
   pri := nil;
-  inicializarVectorPrecios(vP); //se dispone.
-  inicializarVectorMeses(vM);
-  generarListaViajes(pri); //se dispone.
-  procesarListaViajes(pri,vP,vM);
+  generarLista(pri); //se dispone.
+  cargarVector(vM);
+  procesarLista(pri,vM);
 end.
