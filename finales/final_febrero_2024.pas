53c041ddfb2446fbc30cbbf2e5fe3de120078d6f
Agustín Firpo <agustin_firpo@outlook.com.ar>
2025-04-13 19:12:52 -0300

    final febrero 2024 empezado.

---
+++ finales/final_febrero_2024.pas
@@ -0,0 +1,22 @@
+{1) Una empresa dispone de una estructura de datos con las ventas de su comercio. De cada venta se conoce: numero de venta, cantidad de productos y tipo de pago (efectivo o tarjeta). Se pide implementar un programa que genere una seguda estructura con las ventas cuya cantidad de productos tenga mas digitos pares que impares. En la estructura generada deben quedar almacenadas las ventas de tipo de pago efectivo antes que las de tipo de pago con tarjeta}
+
+{=======================================================================================================}
+
+{2) Dados los siguientes programas indique para cada uno si son válidos o no. Además, analice si considera que el funcionamiento en ambos programas es el mismo o no. JUSTIFIQUE.}
+
+Program uno;
+Type datos = array [1..100] of integer;
+Var d: datos;
+Begin
+  //Operaciones e invocaciones a módulos para cargar y recorrer el vector d
+End;
+
+
+Program dos;
+Var
+d: array [1..100] of integer;
+Begin
+// Operaciones e invocaciones a módulos
+para cargar y recorrer el vector d
+End.
+}
